037ece9916b5c0124fb644d1ebd6f49b701146d1
Lauro2710 <triyin25@gmail.com>
2026-07-13 14:23:24 -0700

    Update Ventas por año.sql

--- SQL/Ventas por año.sql
+++ SQL/Ventas por año.sql
@@ -1,1 +1,5 @@
-select year(orderDate) as Año,sum(quantityOrdered*priceEach) as Total_sales from orders join orderdetails on orders.orderNumber=orderdetails.orderNumber where orders.status not in ("Cancelled","Disputed") group by year(orderDate)
+select year(orderDate) as Año,sum(quantityOrdered*priceEach) as Total_sales 
+from orders 
+join orderdetails on orders.orderNumber=orderdetails.orderNumber 
+where orders.status not in ("Cancelled","Disputed") 
+group by year(orderDate)
